8f9820218044cfe732893f790db0fc0387dcbc1c
Jim Yang <jimy@vmware.com>
2020-09-29 12:35:47 -0400

    update dependencies

--- templating/tag_contributions/build_java.sh
+++ templating/tag_contributions/build_java.sh
@@ -16,4 +16,4 @@ else
   ./gradlew build publishToMavenLocal
 fi
 
-pushd build/libs; rm -f jsonteng-contribs.jar; ln -s jsonteng-contribs-${JSONTENG_BUILD_VERSION}.jar jsonteng-contribs.jar; popd
+pushd build/libs; rm -f jsonteng-contribs.jar; ln -s jsonteng-contribs-${JSONTENG_CONTRIBS_BUILD_VERSION}.jar jsonteng-contribs.jar; popd
